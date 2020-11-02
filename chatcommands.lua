cb3a312a3a7d424ba27b7492f2eb093523ef7486
Peter Nerlich <peter.nerlich+dev@googlemail.com>
2020-11-02 18:26:37 +0100

    add /contacts and /compose commands to quickly access contacts book or compose a new mail

--- chatcommands.lua
+++ chatcommands.lua
@@ -4,3 +4,17 @@ minetest.register_chatcommand("mail",{
 		mail.show_inbox(name)
 	end
 })
+
+minetest.register_chatcommand("contacts",{
+	description = "Open the contacts book",
+	func = function(name)
+		mail.show_contacts(name)
+	end
+})
+
+minetest.register_chatcommand("compose",{
+	description = "Compose a mail",
+	func = function(name)
+		mail.show_compose(name, nil, nil, nil, nil, name)
+	end
+})
